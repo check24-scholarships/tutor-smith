3f5149eb2cceb0f38beb3f5e642df210cf056c71
Shizhe He <61113049+h3seas0n@users.noreply.github.com>
2021-08-30 23:57:34 +0200

    Resolved merge conflicts

--- user_management/views.py
+++ user_management/views.py
@@ -1,4 +1,4 @@
-#HTML Handeling
+# HTML Handeling
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
@@ -6,9 +6,12 @@ from django.contrib import messages
 from django.utils.text import slugify
 from django.utils import timezone
 
-#Auth Handler
+# Auth Handler
 from django.contrib.auth.hashers import make_password, check_password
-from django.contrib.auth.password_validation import validate_password, password_changed
+from django.contrib.auth.password_validation import (
+    validate_password,
+    password_changed,
+)
 from random import randint
 
 from django.contrib.auth.forms import PasswordResetForm
@@ -18,7 +21,8 @@ from django.contrib.auth.tokens import default_token_generator
 
 from .forms import *
 from .models import User, Info, Review
-from .validators import validate_register
+from .validators import validate_login, validate_register
+from .choices import *
 
 # TODO: Write functional Ipgrabber eg. with django-ipware
 def get_client_ip(request):
@@ -32,6 +36,14 @@ def get_client_ip(request):
     return ip
 
 
+dict_gender = dict(choice_gender)
+
+
+def index(request):
+    __context = {'users': User.objects.all()}
+    return render(request, 'index.html', __context)
+
+
 def recover_form(request):
     if request.method == 'POST':
         password_reset_form = PasswordResetForm(request.POST)
@@ -43,23 +55,20 @@ def recover_form(request):
             if associated_users.exists():
                 for user in associated_users:"""
             # get users
-            users = User.objects.filter(email=data)
-            if users is not None:
-                for user in users:
-                    print(user)
-                    subject = 'Password Reset Requested'
-                    email_template_name = (
-                        'main/password/password_reset_email.txt'
-                    )
-                    """c = {
-                        "email": "test@gmail.com",
-                        'domain': '127.0.0.1:8000',
-                        'site_name': 'Website',
-                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
-                        "user": "test",
-                        'token': default_token_generator.make_token(user),
-                        'protocol': 'http',
-                    }"""
+            user = User.objects.get(email=data)
+            user = None
+            print(user)
+            subject = 'Password Reset Requested'
+            email_template_name = 'main/password/password_reset_email.txt'
+            """c = {
+                "email": "test@gmail.com",
+                'domain': '127.0.0.1:8000',
+                'site_name': 'Website',
+                "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                "user": "test",
+                'token': default_token_generator.make_token(user),
+                'protocol': 'http',
+            }"""
             # email = render_to_string(email_template_name, c)
             return redirect('/password_reset/done/')
 
@@ -77,43 +86,71 @@ def recover_form_sent(request):
 
 def hello_world_temp(request):
     return render(
-        request, 'hello.html', context={'time': timezone.now(), 'ip':get_client_ip(request)}
+        request,
+        'hello.html',
+        context={'time': timezone.now(), 'ip': get_client_ip(request)},
     )
 
 
 # Handels incoming GET & POST requests on the register view.
 def register(request):
-    #All context gets initialized
-    __context = {'form':None}
-    if request.method == "POST":
+    # All context gets initialized
+    __context = {'form': None}
+    if request.method == 'POST':
         form = UserForm(request.POST)
-        
+
         res = validate_register(request, form)
         if res:
             user = User.objects.create(
-            email=form.cleaned_data['email'],
-            first_name=form.cleaned_data['first_name'],
-            last_name=form.cleaned_data['last_name'],
-            password=make_password(form.cleaned_data['password_1'], salt=str(randint(0, 2**15))),
-            gender=form.cleaned_data['gender'],
-            adress=form.cleaned_data['adress'],
-            phone=form.cleaned_data['phone'],
-            user_class=form.cleaned_data['user_class'],
-            description='',
-            birth_date=form.cleaned_data['birth_date'],
-            ip=get_client_ip(request),
-            is_active=True,
-            is_staff=False,
-            is_admin=False,
-
-            certificate=None,
-            profile_pic=None
+                email=form.cleaned_data['email'],
+                first_name=form.cleaned_data['first_name'],
+                last_name=form.cleaned_data['last_name'],
+                password=make_password(
+                    form.cleaned_data['password_1'],
+                    salt=str(randint(0, 2 ** 15)),
+                ),
+                gender=form.cleaned_data['gender'],
+                adress=form.cleaned_data['adress'],
+                phone=form.cleaned_data['phone'],
+                user_class=form.cleaned_data['user_class'],
+                description='',
+                birth_date=form.cleaned_data['birth_date'],
+                ip=get_client_ip(request),
+                is_active=True,
+                is_staff=False,
+                is_admin=False,
+                certificate=None,
+                profile_pic=None,
             )
             # TODO: Create Context
-            request.session["user"] = user.id
-            messages.add_message(request, messages.SUCCESS, "Nutzer erstellt!")
-            return redirect("/login")
+            request.session['user'] = user.id
+            messages.add_message(request, messages.SUCCESS, 'Nutzer erstellt!')
+            return redirect('/login')
     # Displays Form and Context on view when not returned before
     form = UserForm()
     __context['form'] = form
     return render(request, 'register.html', context=__context)
+
+
+'''
+# XXX: DELETE THIS NOT MY JOB
+def login(request):
+    __context = {"form": None}
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if validate_login(request, form):
+            # return redirect("/dashboard")
+            return HttpResponse("Login!")
+    form = LoginForm()
+    __context['form'] = form
+    return render(request, 'login.html', __context)
+'''
+
+
+def user_profile(request, user_id):
+    __context = {'user': None}
+    # TODO: use Hashid to hide Primary key
+    print(user_id)
+    __context['user'] = get_object_or_404(User, id=user_id)
+    __context['gender'] = dict_gender[__context['user'].gender]
+    return render(request, 'profile.html', __context)
