d6557563128b84c5fb1b6df74b09357c8bd4d256
Shizhe He <61113049+h3seas0n@users.noreply.github.com>
2021-10-02 15:48:26 +0200

    Added offers to search

--- tutor_smith/settings.py
+++ tutor_smith/settings.py
@@ -16,7 +16,6 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
 # load_dotenv(find_dotenv())
 
 # Quick-start development settings - unsuitable for production
@@ -142,7 +141,6 @@ AUTH_USER_MODEL = 'user_management.UserAuth'
 PHONENUMBER_DB_FORMAT = 'INTERNATIONAL'
 
 HASHIDS_SALT = os.getenv('HASHIDS_SALT')
-print(HASHIDS_SALT)
 RESET_HASHIDS_SALT = os.getenv('RESET_HASHIDS_SALT')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
