be38fcaebac4a5320b50ae6d0fd151153fd295d5
loumoe <moelmlouisa@gmail.com>
2021-10-17 18:59:06 +0200

    changed aussehen der angebote in search

--- user_management/choices.py
+++ user_management/choices.py
@@ -2,17 +2,17 @@
 # Use (id, human readable name) the id will be saved in the db
 # TODO: Add subjects
 choice_subject = (
-    (0, 'German'),
-    (1, 'Math'),
-    (2, 'English'),
-    (3, 'Chemestry'),
-    (4, 'Computer Science'),
+    (0, 'Deutsch'),
+    (1, 'Mathe'),
+    (2, 'Englisch'),
+    (3, 'Chemie'),
+    (4, 'Informatik'),
 )
 choice_difficulity = ((0, 'Beginner'), (1, 'Medium'), (2, 'Hard'))
 
 choice_gender = (
     (9, 'Not Stated'),
-    (1, 'Male'),
-    (2, 'Female'),
+    (1, 'Männlich'),
+    (2, 'Weiblich'),
     (3, 'Another Term'),
 )
